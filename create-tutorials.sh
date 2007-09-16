107c94aecd63d2025ee58bfc815d9cfc80115047
mraible <mraible@2409b7a4-2524-0410-a366-655b7483b231>
2007-09-16 03:47:42 +0000

    Upgraded from M5 to 2.0 SNAPSHOT

--- create-tutorials.sh
+++ create-tutorials.sh
@@ -1,8 +1,8 @@
 mkdir temp;cd temp
-mvn archetype:create -DarchetypeGroupId=org.appfuse -DarchetypeArtifactId=appfuse-basic-jsf -DremoteRepositories=http://static.appfuse.org/repository -DarchetypeVersion=2.0-m5 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-jsf
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-jsf  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-jsf
 
-mvn archetype:create -DarchetypeGroupId=org.appfuse -DarchetypeArtifactId=appfuse-basic-spring -DremoteRepositories=http://static.appfuse.org/repository -DarchetypeVersion=2.0-m5 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-spring
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-spring  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-spring
 
-mvn archetype:create -DarchetypeGroupId=org.appfuse -DarchetypeArtifactId=appfuse-basic-struts -DremoteRepositories=http://static.appfuse.org/repository -DarchetypeVersion=2.0-m5 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-struts2
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-struts  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-struts2
 
-mvn archetype:create -DarchetypeGroupId=org.appfuse -DarchetypeArtifactId=appfuse-basic-tapestry -DremoteRepositories=http://static.appfuse.org/repository -DarchetypeVersion=2.0-m5 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-tapestry
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-tapestry  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-tapestry
