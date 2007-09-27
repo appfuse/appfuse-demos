45e12b1c3a28d75ec8da87139ce07351fb4776e3
mraible <mraible@2409b7a4-2524-0410-a366-655b7483b231>
2007-09-27 18:23:03 +0000

    Upgraded to AppFuse 2.0 Final

--- create-tutorials.sh
+++ create-tutorials.sh
@@ -1,8 +1,8 @@
 mkdir temp;cd temp
-mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-jsf  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-jsf
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-jsf  -DarchetypeVersion=2.0 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-jsf
 
-mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-spring  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-spring
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-spring  -DarchetypeVersion=2.0 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-spring
 
-mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-struts  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-struts2
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-struts  -DarchetypeVersion=2.0 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-struts2
 
-mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-tapestry  -DarchetypeVersion=2.0-SNAPSHOT -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-tapestry
+mvn archetype:create -DarchetypeGroupId=org.appfuse.archetypes -DarchetypeArtifactId=appfuse-basic-tapestry  -DarchetypeVersion=2.0 -DgroupId=org.appfuse.tutorial -DartifactId=tutorial-tapestry
